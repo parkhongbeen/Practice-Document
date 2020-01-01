97ef45e1692f5450593c25402305537902475657
parkhongbeen <pack122@naver.com>
2020-01-01 16:50:23 +0900

    choices주석 추가

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'manytomany',
     'myapp.apps.MyappConfig',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
--- myapp/models.py
+++ myapp/models.py
@@ -7,6 +7,9 @@ class Person(models.Model):
         ('M', 'Medium'),
         ('L', 'Large'),
     )
+    # choice의 DB같은 tuple의 0번째 값
+    # tuple의 1번째 값을 가져오고 싶으면, get_FOO_display()를 사용
+    # FOO: Model의 choices를 가진 field명
     shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
