441c1c1b21c6b66e1a01eea7487f6dffdfd98e58
parkhongbeen <pack122@naver.com>
2020-01-04 18:30:20 +0900

    proxy model

--- inheritance/proxy/models.py
+++ inheritance/proxy/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseUser(models.Model):
+    is_superuser = models.BooleanField(default=False)
+
+    def show_pk(self):
+        print(self.pk)
+
+
+class NormalUser(BaseUser):
+    class Meta:
+        proxy = True
+
+
+# SuperUser모델에서 사용할 Manager
+class SuperUserManager(models.Manager):
+    def get_queryset(self):
+        # 기본 QuerySet을 제한
+        return super().get_queryset().filter(is_superuser=True)
+
+
+class SuperUser(BaseUser):
+    # 프록시 모델에 Manager를 지정
+    objects = SuperUserManager()
+
+    class Meta:
+        proxy = True
+
+    def delete_user(self, pk):
+        BaseUser.objects.get(pk=pk).delete()
